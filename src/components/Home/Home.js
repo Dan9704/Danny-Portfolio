f1940a629d433acddd506237d8402ad6dfeaa674
Danny Nguyen <tungduong9704@gmail.com>
2025-12-01 11:32:47 +1100

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,7 +21,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'm
-                <strong className="main-name"> Danny Nguyen</strong>
+                <strong className="main-name"> Danny</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
